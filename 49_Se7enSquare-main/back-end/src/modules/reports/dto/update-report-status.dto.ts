a23d0876b5177794ad841882fb54f052dd07625f
Anant-Garg-ag <garganant021@gmail.com>
2026-05-02 18:35:25 +0530

    Delete 49_Se7enSquare-main directory

--- 49_Se7enSquare-main/back-end/src/modules/reports/dto/update-report-status.dto.ts
+++
@@ -1,9 +0,0 @@
-import { ApiProperty } from '@nestjs/swagger';
-import { IsEnum } from 'class-validator';
-import { ReportStatus } from './report.dto';
-
-export class UpdateReportStatusDto {
-  @ApiProperty({ enum: ReportStatus, example: ReportStatus.REVIEWED })
-  @IsEnum(ReportStatus)
-  status!: ReportStatus;
-}
